ef76cd684d774ebf84b9ef733dfa181131df9a55
Wade Jensen <wadejensenwork@gmail.com>
2018-03-04 10:16:05 +1100

    project structure and test case harnesses

---
+++ FSharpImpureSegmenter/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let x = 5
+    printfn "%A" x
+    //raise (System.NotImplementedException())
+    // Fixme: add implementation here
+    0 // return an integer exit code
---
+++ FSharpPureSegmenter/SegmentModule.fs
@@ -0,0 +1,31 @@
+﻿module SegmentModule
+open System
+open System.Security.Cryptography
+
+type Coordinate = (int * int) // x, y coordinate of a pixel
+type Colour = byte list       // one entry for each colour band, typically: [red, green and blue]
+
+type Segment = 
+    | Pixel of Coordinate * Colour
+    | Parent of Segment * Segment
+
+    
+// determine the cost of merging the given segments: 
+// equal to the standard deviation of the combined the segments minus the sum of the standard deviations of the individual segments, 
+// weighted by their respective sizes and summed over all colour bands
+let mergeCost segment1 segment2 : float = 
+    raise (System.NotImplementedException())
+    // Fixme: add implementation here
+
+// return a list of the standard deviations of the pixel colours in the given segment
+// the list contains one entry for each colour band, typically: [red, green and blue]
+let stddev (segment: Segment): float list =
+    raise (System.NotImplementedException())
+    // Fixme: add implementation here
+    
+    
+    
+    
+    
+    
+    
